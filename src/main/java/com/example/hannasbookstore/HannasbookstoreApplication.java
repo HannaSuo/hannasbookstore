470c730982c60f601c985d60f5c2a89093aa6da4
Hanna Suojanen <hanna.s89@gmail.com>
2022-09-01 12:53:12 +0300

    first commit

---
+++ src/main/java/com/example/hannasbookstore/HannasbookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.hannasbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HannasbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HannasbookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hannasbookstore/HannasbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hannasbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HannasbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
