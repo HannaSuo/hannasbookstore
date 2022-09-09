4e45c44e0f29e790f8686d9630b6a05754364ad3
Hanna Suojanen <hanna.s89@gmail.com>
2022-09-09 19:30:01 +0300

    delete added

--- src/main/java/com/example/hannasbookstore/web/BookController.java
+++ src/main/java/com/example/hannasbookstore/web/BookController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.hannasbookstore.model.Book;
 import com.example.hannasbookstore.model.BookRepository;
 
 @Controller
@@ -18,4 +21,24 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	@GetMapping("/addbook") 
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@PostMapping("/save")
+	public String save(Book book){
+	 repository.save(book);
+	 return "addbook";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteStudent(@PathVariable("id") Long id, Model model){ 
+		repository.deleteById(id);
+	 return "redirect:../booklist";
+	}
+	
+	
 }
